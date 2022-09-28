ddab25f44eb0f59700c8e80e84be3b5e94d81123
Cheng Lim <wenyong.lim@icloud.com>
2022-09-28 15:04:20 +0100

    Completed 03_reverseString passed 4/4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  const reverse = str.split('').reverse().join('');
+  return reverse;
 };
 
 // Do not edit below this line
