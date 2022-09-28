ffc545a27b2ad213c0c9f62229f748aaa3d630e5
Cheng Lim <wenyong.lim@icloud.com>
2022-09-28 14:38:36 +0100

    Created repeat loop for "hey"

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,8 +1,10 @@
 const repeatString = function (str, num) {
-  for (let i = 0; i < num; i++) {
-    text + str;
+  for (let count = 0; count < num + 1; count++) {
+    str.repeat(count);
   }
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
