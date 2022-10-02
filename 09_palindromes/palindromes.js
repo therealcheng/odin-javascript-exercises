c24ca24d8d8915c3636a081ec5f28e95b64e486e
FAMILY-PC\wenyo <wenyong.lim@icloud.com>
2022-10-03 00:07:40 +0100

    09 - Format word

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,7 @@
 const palindromes = function (word) {
+  const formatWord = word.replace('!', '');
   // separate word into split array, reverse array, join array
-  const reverseWord = word.split('').reverse().join('');
+  const reverseWord = word.toLowerCase().split('').reverse().join('');
   if (word === reverseWord) return true;
 };
 
