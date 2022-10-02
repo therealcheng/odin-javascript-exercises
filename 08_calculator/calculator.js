0481e388e4f05a3510776e7cc9af41afd227f54f
Cheng Lim <wenyong.lim@icloud.com>
2022-10-02 17:00:12 +0100

    08 - Add, Subtract and Sum

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,11 +1,21 @@
 // The goal for this exercise is to create a calculator that does the following:
 // add, subtract, get the sum, multiply, get the power, and find the factorial
 
-const add = function () {};
+const add = function (x, y) {
+  return x + y;
+};
 
-const subtract = function () {};
+const subtract = function (x, y) {
+  return x - y;
+};
 
-const sum = function () {};
+const sum = function (array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
+};
 
 const multiply = function () {};
 
