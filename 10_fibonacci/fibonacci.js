f31f04e9906088695e2a70592d3ef42bd62561a5
Cheng Lim <wenyong.lim@icloud.com>
2022-10-03 14:13:30 +0100

    10 - defining sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function (sequence) {
+  let firstSequence = 1;
+  let previousSequence = 1;
+  if (sequence === String) return sequence;
+  else {
+    for (let i = 0; i < sequence.length; i++) {}
+  }
 };
 
 // Do not edit below this line
