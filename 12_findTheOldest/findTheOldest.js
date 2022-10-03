8145df09bce0f7084cbc72443077360c45d6c89c
Cheng Lim <wenyong.lim@icloud.com>
2022-10-03 20:51:37 +0100

    12 - passed test 1

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let yearsLived = 0,
+    oldestNumber = 0,
+    oldestName = '',
+    arrayN = -1;
 
+  // return name that is older
+  people.forEach((person) => {
+    yearsLived = person.yearOfDeath - person.yearOfBirth;
+    person.yearsLived = yearsLived;
+    if (person.hasOwnProperty('yearOfDeath')) arrayN++;
+    else if (yearsLived > oldestNumber) {
+      oldestNumber = yearsLived;
+      oldestName = person.name;
+      arrayN++;
+    }
+  });
+  //   console.log(oldestNumber);
+  //   console.log(oldestName);
+  return people[arrayN];
 };
 
 // Do not edit below this line
