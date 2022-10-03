0e44d6ef0225a4a267f99a3268a194a3bc310831
FAMILY-PC\wenyo <wenyong.lim@icloud.com>
2022-10-03 13:12:58 +0100

    9 - Replace '!'

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,7 +1,7 @@
 const palindromes = function (word) {
   const formatWord = word.replace('!', '');
   // separate word into split array, reverse array, join array
-  const reverseWord = word.toLowerCase().split('').reverse().join('');
+  const reverseWord = formatWord.toLowerCase().split('').reverse().join('');
   if (word === reverseWord) return true;
 };
 
