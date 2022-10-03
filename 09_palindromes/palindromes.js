92e711395cf72cda9e652bdaaa2cbb609c47ad08
Cheng Lim <wenyong.lim@icloud.com>
2022-10-03 12:57:57 +0100

    Revert "Some awesome title"
    This reverts commit 68a2e195fcff89684b8f5f7babc8b334d5a540d3.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,6 @@
 const palindromes = function (word) {
   // separate word into split array, reverse array, join array
-   const reverseWord = word.split('').reverse().join('');
+  const reverseWord = word.split('').reverse().join('');
   if (word === reverseWord) return true;
 };
 
