8c8bc5c65b80b0838df5ed1a94dffda7cdfcd998
Cheng Lim <wenyong.lim@icloud.com>
2022-10-03 13:36:51 +0100

    Completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,8 +1,9 @@
 const palindromes = function (word) {
-  const formatWord = word.replace('!', '').toLowerCase();
+  const formatWord = word.toLowerCase().replace(/[!., ]/g, '');
   // separate word into split array, reverse array, join array
   const reverseWord = formatWord.toLowerCase().split('').reverse().join('');
   if (formatWord === reverseWord) return true;
+  else return false;
 };
 
 // Do not edit below this line
